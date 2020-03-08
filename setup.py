2bc406439f2b6ac753b2cb913d6a23df1e088183
_Hacker_ <bulat.shaekhov@gmail.com>
2020-03-08 14:36:09 +0300

    upgrade setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,55 @@
 from setuptools import setup, find_packages
 
+def get_reqs(file_path):
+    from pip._internal.req import parse_requirements
+
+    def clear_titles(title):
+        import re
+        return re.sub('[\t\r\n]', '', title).strip()
+
+    reqs = open(file_path).read().replace('\n\r', '\n').split('\n')
+    req = list(parse_requirements(file_path, session='hack'))
+
+    ans = []
+
+    minus_idx = 0
+
+    for idx, el in enumerate(reqs):
+
+        el = clear_titles(el)
+        if len(el) < 1 or (len(el) > 0 and el[0] == '#'):
+            minus_idx += 1
+            continue
+
+        egg = False
+
+        try:
+            eqq_idx = el.rindex('#egg=') + 5
+            final_idx = eqq_idx
+            egg = True
+        except ValueError:
+            eqq_idx = -1
+
+        if not egg:
+
+            if req[idx - minus_idx].name:
+                ans.append(el)
+                continue
+
+            try:
+                rsidx = el.rindex('/') + 1
+            except ValueError:
+                rsidx = -1
+            try:
+                lsidx = el.rindex('\\') + 1
+            except ValueError:
+                lsidx = -1
+            final_idx = max(rsidx, lsidx)
+        name = el[final_idx:]
+
+        ans.append(name + ' @ ' + el)
+    return ans
+
 setup(
     name="time_tools",
     version='0.0.1',
@@ -24,8 +74,8 @@ setup(
     author_email='bulat.shaekhov@gmail.com',
     url='https://github.com/IHackerI/time_tools',
     packages=find_packages(),
-    install_requires=[]
-        #open('requirements.txt').read().split('\n\r|\n')
+    install_requires=
+            get_reqs('requirements.txt')
     ,
     include_package_data=True,
     zip_safe=False,
